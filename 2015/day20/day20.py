b8eb2d93b21a06433eff5fa23c33c3d96c81d50d
kyleyarwood <yarwoodkyle@gmail.com>
2020-12-05 01:41:18 -0500

    Finished day 1 2020

--- 2015/day20/day20.py
+++ 2015/day20/day20.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 PROBLEM_INPUT = 34000000
 
 def get_lowest_house_with_at_least_n_presents(n):
@@ -7,11 +8,40 @@ def get_lowest_house_with_at_least_n_presents(n):
             houses[j - 1][1] += i*10
     return list(filter(lambda x: x[1] >= n, houses))[0][0]
 
+def part2(n):
+    houses = defaultdict(int)
+
+    i = 1
+    upper_bound = -1
+    while True:
+        for j in range(1, 51):
+            houses[i*j] += i*11
+            if houses[i*j] >= n:
+                upper_bound = i*j
+                break
+        else:
+            i += 1
+            continue
+        i += 1
+        break
+    
+    while i < upper_bound//11 + 1:
+        for j in range(1, 51):
+            if j >= upper_bound//11 + 1:
+                break
+            houses[i*j] += i*11
+        i += 1
+
+    return min(list(filter(lambda x: houses[x] >= n, houses.keys())))
 
 def main():
     global PROBLEM_INPUT
-    result = get_lowest_house_with_at_least_n_presents(PROBLEM_INPUT)
+    #result = get_lowest_house_with_at_least_n_presents(PROBLEM_INPUT)
     #part 1
+    #print(result)
+
+    result = part2(PROBLEM_INPUT)
+    #part 2
     print(result)
 
 
